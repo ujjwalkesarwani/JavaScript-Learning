d01609d30e0ae32d963e967927d3a4be987dea0d
Ujjwalkesarwani <ujjwalkesarwani29@gmail.com>
2026-06-08 20:08:38 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1337;
+let accountEmail = "hello@gmail.com";
+var accountPassword = "1234";
+accountCity = "jaipur";
+
+let accountState; // undefined
+
+// accountId = 2; // error because accountId is a constant variable
+
+accountEmail = "hcc@gmail.com"; 
+accountPassword = "4321";
+accountCity = "delhi";
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3+3); // we are using node.js, not a browser, so alert is not available
+ 
+// Data types
+let name = "John"; // string
+let age = 30; // number
+let isMarried = false; // boolean
+let children = null; // null(object)
+let address; // undefined
+let hobbies = ["reading", "traveling", "coding"]; // array
+let person = { // object
+    name: "John",
+    age: 30,
+    isMarried: false
+};
+console.log(name);
+console.log(age);
+console.log(isMarried);
+console.log(children);
+console.log(address);
+console.log(hobbies);
+console.log(person);
