d9a64a40a7530de7de26a39d4d6f6dc45c7eee0d
Ujjwalkesarwani <ujjwalkesarwani29@gmail.com>
2026-06-14 12:39:56 +0530

    objects part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,38 @@
+// const tinderUser = new Objects()  // singelton
+const tinderUser = {}  //non singelton
+
+tinderUser.id = '123abc';
+tinderUser.name = 'John';
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "john@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "John",
+            lastName: "Doe"
+        }
+    },
+    age: 30
+};
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {1: "one",2: "two"};
+const obj2 = {3: "three",4: "four"};
+
+// const obj3 = {obj1, obj2};
+
+// const obj3 = Object.assign({}, obj1, obj2); 
+
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
+
